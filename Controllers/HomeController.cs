7e57ed66ca6d9ac8909c22782a9bd8423207e52c
Danni Skov Hoeglund <skuffe@pwnz.dk>
2012-05-01 15:14:35 +0200

    Total number of vehicles added to dashboard.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VIS.Models;
 
 namespace VIS.Controllers
 {
@@ -10,7 +11,10 @@ namespace VIS.Controllers
 	{
 		public ActionResult Index()
 		{
+            VISContext dbContext = new VISContext();
 			ViewBag.Message = "Dashboard";
+
+            ViewBag.vehicles = (from v in dbContext.Vehicles select v).Count();
 
 			return View();
 		}
